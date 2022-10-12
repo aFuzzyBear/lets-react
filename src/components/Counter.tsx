c29e966f364f2e6acc1c60fe1aec916d82a9fa2f
aFuzzyBear <afuzzybear@outlook.com>
2022-10-12 14:33:32 +0100

    ⚛ init commit

---
+++ src/components/Counter.tsx
@@ -0,0 +1,15 @@
+import {useState} from 'react'
+
+export const Counter = ()=>{
+    const [count,setCount]= useState(0)
+
+    return(
+        <>
+            <div className="">
+                <button onClick={()=>setCount(count + 1)}>+</button>
+                <p>{count}</p>
+                <button onClick={()=>setCount(count - 1)}>-</button>
+            </div>
+        </>
+    )
+}
